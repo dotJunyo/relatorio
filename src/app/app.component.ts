e8a7dc8f385655632f8f4c40462c81949080bdba
Junyo <junyogabriel001@hotmail.com>
2022-03-07 00:11:41 -0300

    HTML to PNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  title = 'relatorio';
+
+  generateImage() {
+    var node: any = document.getElementById('image-section');
+    htmlToImage
+      .toPng(node)
+      .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        var link = document.createElement('a');
+        link.href = img.src;
+        link.download = 'Relatorio.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+      .catch(function (error) {
+        console.error('Algo deu errado, avise o Junyo#3633!', error);
+      });
+  }
+
   constructor() {}
 }
